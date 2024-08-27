eaab7d8c02605a7195ad400946fdddb7b45b0c1c
Juan Debenedetti <juandebenedetti94@gmail.com>
2024-08-27 20:38:58 -0300

    ProgressBar

--- src/main/java/com/craiver/hellofx/Controller.java
+++ src/main/java/com/craiver/hellofx/Controller.java
@@ -1,23 +1,37 @@
 package com.craiver.hellofx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
-    private Slider volumen;
+    private ProgressBar progressBar;
+    @FXML
+    private Button button;
     @FXML
     private Label label;
 
+    private BigDecimal progress = new BigDecimal(String.format("%.2f", 0.0).replace(",", "."));
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        label.setText(String.valueOf((int )volumen.getValue()));
-        volumen.valueProperty().addListener((observableValue, number, t1) -> label.setText(String.valueOf((int)volumen.getValue())));
+        progressBar.setStyle("-fx-accent: #00FF00;");
     }
+
+    public void increaseProgress(ActionEvent event) {
+        if (progress.doubleValue() < 1) {
+            progress = new BigDecimal(String.format("%.2f", progress.doubleValue() + 0.1).replace(",", "."));
+            progressBar.setProgress(progress.doubleValue());
+            label.setText(progress.doubleValue() * 100 + "%");
+        }
+    }
+
+
 }
