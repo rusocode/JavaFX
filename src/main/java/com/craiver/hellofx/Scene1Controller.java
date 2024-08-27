4dd48ba05627b9a310cdc39c107635b4744752f1
Juan Debenedetti <juandebenedetti94@gmail.com>
2024-08-27 11:55:14 -0300

    Communication between controllers

--- src/main/java/com/craiver/hellofx/Scene1Controller.java
+++ src/main/java/com/craiver/hellofx/Scene1Controller.java
@@ -1,10 +1,12 @@
 package com.craiver.hellofx;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,22 +18,25 @@ import java.util.Objects;
  * La anotacion {@code @FXML} inyecta todos los valores que se encuentran detro del archivo .fxml en el controlador.
  */
 
-public class Controller {
+public class Scene1Controller {
+
+    @FXML
+    TextField nameText;
 
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchScene1(ActionEvent e) throws IOException {
-        root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/scene1.fxml"))));
-        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void login(ActionEvent e) throws IOException {
 
-    public void switchScene2(ActionEvent e) throws IOException {
-        root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/scene2.fxml"))));
+        String username = nameText.getText();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/scene2.fxml"));
+        root = loader.load();
+
+        Scene2Controller scene2Controller = loader.getController();
+        scene2Controller.displayName(username);
+
+        // root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/scene2.fxml"))));
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
