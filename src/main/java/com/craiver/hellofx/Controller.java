c2d8ff6706f1d9739c9650bc30dacca66428d170
Juan Debenedetti <juandebenedetti94@gmail.com>
2024-08-28 10:06:23 -0300

    ListView

--- src/main/java/com/craiver/hellofx/Controller.java
+++ src/main/java/com/craiver/hellofx/Controller.java
@@ -1,37 +1,36 @@
 package com.craiver.hellofx;
 
-import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-import java.math.BigDecimal;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class Controller implements Initializable {
 
     @FXML
-    private ProgressBar progressBar;
-    @FXML
-    private Button button;
+    private ListView<String> listView;
     @FXML
     private Label label;
 
-    private BigDecimal progress = new BigDecimal(String.format("%.2f", 0.0).replace(",", "."));
+    private final String[] food = {"locro", "asado", "milas"};
+
+    String currentFood;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        progressBar.setStyle("-fx-accent: #00FF00;");
+        listView.getItems().addAll(food);
+        // Agrega un oyente al componente ListView
+        listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                currentFood = listView.getSelectionModel().getSelectedItem();
+                label.setText(currentFood);
+            }
+        });
     }
-
-    public void increaseProgress(ActionEvent event) {
-        if (progress.doubleValue() < 1) {
-            progress = new BigDecimal(String.format("%.2f", progress.doubleValue() + 0.1).replace(",", "."));
-            progressBar.setProgress(progress.doubleValue());
-            label.setText(progress.doubleValue() * 100 + "%");
-        }
-    }
-
 
 }
